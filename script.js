7eee8fe55dfbc33f090cb77c7bc84b5faacf579e
Anth0-12 <antho62680@live.fr>
2025-01-05 02:41:07 +0100

    Création slider + fonction avant/après

---
+++ script.js
@@ -0,0 +1,27 @@
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+  });
+
+
+  const images = document.querySelectorAll('.comp-img'); // Sélectionne toutes les div avec la classe "comp-img"
+
+  images.forEach(imgContainer => { // Pour chaque div
+    const hideImage = imgContainer.querySelector('.hide'); // Trouve l'image cachée
+    const visibleImage = imgContainer.querySelector('.visible'); // Trouve l'image visible
+  
+    // Quand la souris entre sur l'image
+    imgContainer.addEventListener('mouseover', function() {
+      hideImage.style.display = "none"; // Cache l'image "hide"
+      visibleImage.style.display = "block"; // Affiche l'image "visible"
+      visibleImage.style.cursor = "pointer"; // Change le curseur en pointer
+    });
+  
+    // Quand la souris sort de l'image
+    imgContainer.addEventListener('mouseout', function() {
+      hideImage.style.display = "block"; // Affiche l'image "hide"
+      visibleImage.style.display = "none"; // Cache l'image "visible"
+    });
+  });
